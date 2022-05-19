dd8b8feb6dec3599df1f2854e2f8101240064da3
Alan Sanchez <realalansan@gmail.com>
2022-05-19 11:06:18 -0700

    Create basic views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name', 'Ecommerce') }}</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    @yield('content')
 </body>
 
 </html>
