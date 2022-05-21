344f8d2a9841586e1ba8d42130a9c59eded8bcb2
Alan Sanchez <realalansan@gmail.com>
2022-05-21 08:31:00 -0700

    Create basic carousel

---
+++ resources/js/carousel.js
@@ -0,0 +1,1 @@
+const carousels = document.getElementsByClassName("carousel");
---
+++ resources/views/components/carousel.blade.php
@@ -0,0 +1,28 @@
+<div class="carousel">
+    <button class="carousel__button carousel__button-left">
+        <img src="" alt="">
+    </button>
+
+    <div class="carousel__track-container">
+        <ul class="carousel__track">
+            <li class="carousel__slide">
+                <img class="carousel__image" src="" alt="">
+            </li>
+            <li class="carousel__slide">
+                <img class="carousel__image" src="
+                    alt="">
+            </li>
+            <li class="  carousel__slide">
+                <img class="carousel__image" src="
+                    alt="">
+            </li>
+        </ul>
+    </div>
+
+    <button class="  carousel__button carousel__button-right"></button>
+                <div class="carousel__nav">
+                    <button class="carousel__indicator">
+                        <img src="" alt="">
+                    </button>
+                </div>
+    </div>
