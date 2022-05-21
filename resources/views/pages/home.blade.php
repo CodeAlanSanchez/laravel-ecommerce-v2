82a5b4b632b6457fec40d5a81ae0629efe3885ab
Alan Sanchez <realalansan@gmail.com>
2022-05-21 15:04:31 -0700

    Create basic home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="banner banner--main">
+        <img src={{ $banner_img }} alt="Banner showing current promotions" class="banner_img">
+    </div>
+    @include('components.carousel')
+    <h3 class="title">
+        title
+    </h3>
+    <div class="products__list">
 
+    </div>
 @endsection
