5590450d20f62d9c2f3ffccc4a3c049bf7c53967
Alan Sanchez <realalansan@gmail.com>
2022-05-21 15:04:43 -0700

    Start carousel JavaScript file

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,1 +1,11 @@
-const carousels = document.getElementsByClassName("carousel");
+const tracks = document.querySelector(".carousel");
+
+const slides = Array.from(track.children);
+
+const nextButton = document.querySelectorAll(
+    ".carousel .carousel__button-right"
+);
+
+const prevButton = document.querySelectorAll(
+    ".carousel .carousel__button-left"
+);
