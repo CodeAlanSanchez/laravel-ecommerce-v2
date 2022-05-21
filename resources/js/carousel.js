e921615be50941f38a2dee02f3154342ebc7a355
Alan Sanchez <realalansan@gmail.com>
2022-05-21 15:49:28 -0700

    Create slide switching functionality

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,11 +1,47 @@
-const tracks = document.querySelector(".carousel");
+const carousel = document.querySelector(".carousel");
+
+const track = document.querySelector(".carousel__track");
+
+const indicatorNav = document.querySelector(".carousel__nav");
 
 const slides = Array.from(track.children);
 
-const nextButton = document.querySelectorAll(
-    ".carousel .carousel__button-right"
-);
+let activeSlide = 0;
 
-const prevButton = document.querySelectorAll(
-    ".carousel .carousel__button-left"
-);
+slides[0].classList.add("active");
+
+const prevButton = document.querySelector(".carousel .carousel__button-left");
+
+const nextButton = document.querySelector(".carousel .carousel__button-right");
+
+prevButton.addEventListener("click", () => {
+    switchSlide(-1);
+});
+
+nextButton.addEventListener("click", () => {
+    switchSlide(1);
+});
+
+const switchSlide = (index) => {
+    console.log(slides);
+    if (index === -1 && activeSlide === 0) {
+        activeSlide = slides.length - 1;
+    } else if (index === 1 && activeSlide === slides.length - 1) {
+        activeSlide = 0;
+    } else {
+        activeSlide += index;
+    }
+    console.log(activeSlide);
+    slides.forEach((slide) => slide.classList.remove("active"));
+    slides[activeSlide].classList.add("active");
+};
+
+(() => {
+    let buttons = "";
+
+    slides.forEach(
+        (slide) => (buttons += '<button class="carousel__indicator"></button>')
+    );
+
+    indicatorNav.innerHTML = buttons;
+})();
