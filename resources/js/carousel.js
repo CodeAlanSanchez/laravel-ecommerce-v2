9075d7c3128369d3be9350597b6e72601e9161da
Alan Sanchez <realalansan@gmail.com>
2022-05-23 14:42:49 -0700

    Add more abstraction and add navigation to dots

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,8 +1,8 @@
 const carousel = document.querySelector(".carousel");
-
 const track = document.querySelector(".carousel__track");
-
 const indicatorNav = document.querySelector(".carousel__nav");
+const prevButton = document.querySelector(".carousel .carousel__button-left");
+const nextButton = document.querySelector(".carousel .carousel__button-right");
 
 const slides = Array.from(track.children);
 
@@ -10,20 +10,47 @@ let activeSlide = 0;
 
 slides[0].classList.add("active");
 
-const prevButton = document.querySelector(".carousel .carousel__button-left");
+const addClickListener = (button, num) => {
+    button.addEventListener("click", () => {
+        switchSlide(num);
+    });
+};
 
-const nextButton = document.querySelector(".carousel .carousel__button-right");
+[
+    { button: prevButton, num: -1 },
+    { button: nextButton, num: 1 },
+].map((i) => addClickListener(i.button, i.num));
 
-prevButton.addEventListener("click", () => {
-    switchSlide(-1);
-});
+const initiateNav = () => {
+    let buttonHTML = '<button class="carousel__indicator"></button>';
+    slides.forEach(() => (indicatorNav.innerHTML += buttonHTML));
+};
 
-nextButton.addEventListener("click", () => {
-    switchSlide(1);
-});
+initiateNav();
 
-const switchSlide = (index) => {
-    console.log(slides);
+const buttons = Array.from(indicatorNav.children);
+
+buttons[activeSlide].classList.add("active");
+
+buttons.forEach((b, index) =>
+    b.addEventListener("click", () => switchToSlide(index))
+);
+
+let activateSlide = () => {
+    slides.forEach((slide) => slide.classList.remove("active"));
+    slides[activeSlide].classList.add("active");
+
+    buttons.map((button) => button.classList.remove("active"));
+    buttons[activeSlide].classList.add("active");
+};
+
+let switchToSlide = (index) => {
+    activeSlide = index;
+
+    activateSlide();
+};
+
+let switchSlide = (index) => {
     if (index === -1 && activeSlide === 0) {
         activeSlide = slides.length - 1;
     } else if (index === 1 && activeSlide === slides.length - 1) {
@@ -31,17 +58,6 @@ const switchSlide = (index) => {
     } else {
         activeSlide += index;
     }
-    console.log(activeSlide);
-    slides.forEach((slide) => slide.classList.remove("active"));
-    slides[activeSlide].classList.add("active");
+
+    activateSlide();
 };
-
-(() => {
-    let buttons = "";
-
-    slides.forEach(
-        (slide) => (buttons += '<button class="carousel__indicator"></button>')
-    );
-
-    indicatorNav.innerHTML = buttons;
-})();
