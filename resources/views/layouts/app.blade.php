c708ceb96432ee55fa29dec4cbe0fcddcf5c6462
Alan Sanchez <realalansan@gmail.com>
2022-05-24 06:57:16 -0700

    Add font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="https://kit.fontawesome.com/81a81a14b1.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
