27e22ad496ee9a2c3d245982be257508400026ac
Alan Sanchez <realalansan@gmail.com>
2022-05-24 06:57:09 -0700

    Create slides based on data and add chevrons

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,28 +1,20 @@
 <div class="carousel">
     <button class="carousel__button carousel__button-left">
-        <img src="" alt="">
+        <i class="fa-solid fa-chevron-left"></i>
     </button>
 
     <div class="carousel__track-container">
         <ul class="carousel__track">
-            <li class="carousel__slide">
-                <img class="carousel__image" src="" alt="">
-            </li>
-            <li class="carousel__slide">
-                <img class="carousel__image" src="
-                    alt="">
-            </li>
-            <li class="  carousel__slide">
-                <img class="carousel__image" src="
-                    alt="">
-            </li>
+            @foreach ($slides as $slide)
+                <li class="carousel__slide">
+                    <img src="{{ $slide . image_url }}" alt="{{ $slide . image_name }}">
+                </li>
+            @endforeach
         </ul>
     </div>
-
-    <button class="  carousel__button carousel__button-right"></button>
-                <div class="carousel__nav">
-                    <button class="carousel__indicator">
-                        <img src="" alt="">
-                    </button>
-                </div>
+    <button class="carousel__button carousel__button-right">
+        <i class="fa-solid fa-chevron-right"></i>
+    </button>
+    <div class="carousel__nav">
     </div>
+</div>
