fa95f8d146c5cbf727ac318de19ad1f930b0bb7a
Alan Sanchez <realalansan@gmail.com>
2022-05-29 14:37:59 -0700

    Add icons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,8 +3,8 @@
         <ul class="navigation__list">
             <li class="navigation__item"><a class="navigation__link" href="/">Home</a></li>
             <li class="navigation__item"><a class="navigation__link" href="/products">All&nbsp;Products</a></li>
-            <li class="navigation__item"><a class="navigation__link" href="/products?tag=men">Mens</a></li>
-            <li class="navigation__item"><a class="navigation__link" href="/products?tag=women">Womens</a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/products/men">Mens</a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/products/women">Womens</a></li>
             <li class="navigation__item"><a class="navigation__link" href="/cart">Cart&nbsp;<i
                         class="fa-solid fa-cart-shopping"></i></a></li>
             <li class="navigation__item"><a class="navigation__link" href="/account">Account&nbsp;<i
