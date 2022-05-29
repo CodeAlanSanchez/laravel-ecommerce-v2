bfb0e87a5407c18a617ac4ad8ecc6878828b7255
Alan Sanchez <realalansan@gmail.com>
2022-05-29 11:56:55 -0700

    Add navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,1 +1,14 @@
-
+<header>
+    <nav class="navigation">
+        <ul class="navigation__list">
+            <li class="navigation__item"><a class="navigation__link" href="/">Home</a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/products">All&nbsp;Products</a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/products?tag=men">Mens</a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/products?tag=women">Womens</a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/cart">Cart&nbsp;<i
+                        class="fa-solid fa-cart-shopping"></i></a></li>
+            <li class="navigation__item"><a class="navigation__link" href="/account">Account&nbsp;<i
+                        class="fa-solid fa-user"></i></a></li>
+        </ul>
+    </nav>
+</header>
