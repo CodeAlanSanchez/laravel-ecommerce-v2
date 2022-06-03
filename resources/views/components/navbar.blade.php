59e628f71b6a43e1887ee1526738194a7a90d85c
Alan Sanchez <realalansan@gmail.com>
2022-06-02 21:30:31 -0700

    Remove underline from links and adjust icon margin

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,9 +5,9 @@
             <li class="navigation__item"><a class="navigation__link" href="/products">All&nbsp;Products</a></li>
             <li class="navigation__item"><a class="navigation__link" href="/products/men">Mens</a></li>
             <li class="navigation__item"><a class="navigation__link" href="/products/women">Womens</a></li>
-            <li class="navigation__item"><a class="navigation__link" href="/cart">Cart&nbsp;<i
+            <li class="navigation__item"><a class="navigation__link" href="/cart">Cart<i
                         class="fa-solid fa-cart-shopping"></i></a></li>
-            <li class="navigation__item"><a class="navigation__link" href="/account">Account&nbsp;<i
+            <li class="navigation__item"><a class="navigation__link" href="/account">Account<i
                         class="fa-solid fa-user"></i></a></li>
         </ul>
     </nav>
