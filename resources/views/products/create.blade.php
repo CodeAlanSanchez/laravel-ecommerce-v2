06c9d8c893b96ff0dfea045b53be80839d1bf6e0
Alan Sanchez <realalansan@gmail.com>
2022-06-10 13:20:32 -0700

    Add error section

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert--danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class="form__container">
-        <form action="/p" enctype="multipart/form-data" method="post">
+        <form class="form" action="/products" enctype="multipart/form-data" method="post">
             @csrf
 
             <div class="input required">
@@ -11,19 +20,19 @@
             </div>
             <div class="input required">
                 <label for="desc">Description</label>
-                <input name="desc" type="text" placeholder="Description">
+                <textarea name="desc" id="desc" cols="30" rows="3" placeholder="Product Description"></textarea>
             </div>
             <div class="input required">
                 <label for="price">Price</label>
                 <input name="price" type="text" placeholder="Price">
             </div>
+            <div class="input">
+                <label for="discount">Discount</label>
+                <input name="discount" type="text" placeholder="Discount (optional)">
+            </div>
             <div class="input required">
                 <label for="image">Image</label>
                 <input name="image" type="file" placeholder="Image">
-            </div>
-            <div class="input">
-                <label for="discount">Discount</label>
-                <input name="discount" type="file" placeholder="Discount (optional)">
             </div>
             <div class="input">
                 <label for="gender">Gender</label>
@@ -33,6 +42,8 @@
                     <option value="unisex">Unisex</option>
                 </select>
             </div>
+            <button type="submit" class="button button--neutral outline">Submit</button>
+            <input type="hidden" name="_token" value="{{ Session::token() }}">
         </form>
     </div>
 @endsection
