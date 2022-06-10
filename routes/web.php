950b37051fcdacc4cf6cc82e86ee131d623dea9d
Alan Sanchez <realalansan@gmail.com>
2022-06-09 19:00:10 -0700

    Convert individual routes to resource controller

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
-Route::get('/products/{group?}', [\App\Http\Controllers\ProductController::class, 'index']);
-Route::get('/products/create', [\App\Http\Controllers\ProductController::class, 'create']);
+Route::resource('products', ProductController::class);
